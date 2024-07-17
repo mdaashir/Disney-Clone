c4b63763334914fb80d09a2cf1322b819dc14878
mdaashir <s.mohamedaashir@gmail.com>
2024-07-17 19:17:05 +0530

    Added api call for genre id

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
 const movieBaseUrl = 'https://api.themoviedb.org/3';
+const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
 const api_key = '21453a69eabf45f0a56b51202e353808';
-const getTrendingVideos = axios.get(
-	movieBaseUrl + '/trending/all/day?api_key=' + api_key
-);
+const trendingVideosBaseUrl =
+	movieBaseUrl + '/trending/all/day?api_key=' + api_key;
+const movieByGenreBaseUrl = movieBaseUrl + '/discover/movie?api_key=' + api_key;
+const getTrendingVideos = axios.get(trendingVideosBaseUrl);
+const getMovieByGenreId = (id) =>
+	axios.get(movieByGenreBaseUrl + '&with_genres=' + id);
 
-export default { getTrendingVideos };
+export default {
+	getTrendingVideos,
+	getMovieByGenreId,
+	imageBaseUrl,
+};
