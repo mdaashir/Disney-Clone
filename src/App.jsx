95234a810e600df8ae78676c02002a4b5decd73b
mdaashir <22pt17@psgtech.ac.in>
2025-03-04 21:59:52 +0530

    old changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import React from 'react';
 import Header from './Components/Header';
 import Slider from './Components/Slider';
-
 import ProductionHouse from './Components/ProductionHouse';
 import GenreMovieList from './Components/GenreMovieList';
 
