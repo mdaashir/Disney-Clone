f27a94d7d0e25c58b2207f590d0959047748b40f
mdaashir <22pt17@psgtech.ac.in>
2025-08-09 09:07:44 +0530

    feat: introduce react-query, env-based API key, refactor fetching with loading/error states

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,14 +1,28 @@
 import axios from 'axios';
 
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const movieBaseUrl = 'https://api.themoviedb.org/3';
-const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
-const api_key = '21453a69eabf45f0a56b51202e353808';
-const trendingVideosBaseUrl =
-	movieBaseUrl + '/trending/all/day?api_key=' + api_key;
-const movieByGenreBaseUrl = movieBaseUrl + '/discover/movie?api_key=' + api_key;
-const getTrendingVideos = axios.get(trendingVideosBaseUrl);
-const getMovieByGenreId = (id) =>
-	axios.get(movieByGenreBaseUrl + '&with_genres=' + id);
+export const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
+
+// Axios instance for TMDB
+const apiClient = axios.create({
+	baseURL: movieBaseUrl,
+	params: {
+		api_key: API_KEY,
+	},
+});
+
+apiClient.interceptors.response.use(
+	(res) => res,
+	(err) => {
+		// You can enhance logging here
+		return Promise.reject(err);
+	}
+);
+
+export const getTrendingVideos = () => apiClient.get('/trending/all/day');
+export const getMovieByGenreId = (id) =>
+	apiClient.get('/discover/movie', { params: { with_genres: id } });
 
 export default {
 	getTrendingVideos,
